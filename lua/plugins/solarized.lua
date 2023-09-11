8cbdb0eda883c55d6ad16970c6584de0dc4da232
Abraham Alcaina <abraham.alcaina@gmail.com>
2023-09-11 09:52:31 +0200

    fix: for now disasable solarized to test tokuionight-moon

--- lua/plugins/solarized.lua
+++ lua/plugins/solarized.lua
@@ -1,11 +1,13 @@
+-- for now disable it, testing tokonight
+if true then return {} end
+
 return {
-    {
-        -- Solarize color scheme
-        'shaunsingh/solarized.nvim',
-        priority = 1000,
-        config = function()
-          vim.opt.background="light"
-          vim.cmd.colorscheme 'solarized'
-        end,
-      },
+  {
+    -- Solarize color scheme
+    'shaunsingh/solarized.nvim',
+    priority = 1000,
+    config = function()
+      vim.opt.background = "light"
+    end,
+  },
 }
