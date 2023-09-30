7d139c03165bf7f9b10f93f1928cb0146e030e34
Abraham Alcaina <abraham.alcaina@gmail.com>
2023-09-30 10:52:38 +0200

    fix: do not load solarized if we don't use it

--- lua/plugins/solarized.lua
+++ lua/plugins/solarized.lua
@@ -1,10 +1,12 @@
 -- for now disable it, testing tokonight
-if true then return {} end
+if true then
+  return {}
+end
 
 return {
   {
     -- Solarize color scheme
-    'shaunsingh/solarized.nvim',
+    "shaunsingh/solarized.nvim",
     priority = 1000,
     config = function()
       vim.opt.background = "light"
