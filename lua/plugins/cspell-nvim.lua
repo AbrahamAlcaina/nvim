d9d522aba25fa96b60f18b9560a43d27dfcabf0f
Abraham Alcaina <abraham.alcaina@gmail.com>
2023-09-30 10:50:45 +0200

    chore: package upgrade + fix

--- lua/plugins/cspell-nvim.lua
+++ lua/plugins/cspell-nvim.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     event = "BufReadPre",
     dependencies = { "davidmh/cspell.nvim" },
     opts = function(_, opts)
