dbadefa646d71109f7142fccc0e2024cda7980c6
Abraham Alcaina <abraham.alcaina@gmail.com>
2023-10-19 19:20:22 +0200

    fix: cspell

--- lua/plugins/cspell-nvim.lua
+++ lua/plugins/cspell-nvim.lua
@@ -4,7 +4,7 @@ return {
     event = "BufReadPre",
     dependencies = { "davidmh/cspell.nvim" },
     opts = function(_, opts)
-      local nls = require("null-ls")
+      -- local nls = require("null-ls")
       local cspell = require("cspell")
       local cspell_config = vim.fn.expand("$HOME/.config/cspell.json")
       local shared_config = {
@@ -12,7 +12,7 @@ return {
           return cspell_config
         end,
       }
-      opts.sources = vim.list_extend(opts.sources, {
+      opts.sources = vim.list_extend(opts.sources or {}, {
         cspell.diagnostics.with({ config = shared_config }),
         cspell.code_actions.with({ config = shared_config }),
       })
