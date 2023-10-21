9200da2d1a7e923574d3f05269910f51b8c6e1f2
Abraham Alcaina <abraham.alcaina@gmail.com>
2023-10-21 13:09:10 +0200

    feat: add haskell config

---
+++ lua/plugins/haskell.lua
@@ -0,0 +1,9 @@
+return {
+  'mrcjkb/haskell-tools.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim', "nvim-telescope/telescope.nvim"
+  },
+  version = '^2', -- Recommended
+  ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
+
+}
