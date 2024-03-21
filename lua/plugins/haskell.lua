d232640e7f85596b533605a7eb4f2721db7984a4
Abraham Alcaina <abraham.alcaina@gmail.com>
2024-03-21 08:33:10 +0100

    fix: haskell pluging

--- lua/plugins/haskell.lua
+++ lua/plugins/haskell.lua
@@ -1,3 +1,14 @@
+vim.g.haskell_tools = {
+  tools = {
+    codeLens = {
+      autoRefresh = true,
+    },
+    hoogle = { mode = "telescope-local" },
+    hover = { enable = true },
+    fastTags = { enable = true },
+  },
+}
+
 return {
   'mrcjkb/haskell-tools.nvim',
   dependencies = {
@@ -7,3 +18,4 @@ return {
   ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
 
 }
+-- ~/.config/nvim/after/ftplugin/haskell.lua
